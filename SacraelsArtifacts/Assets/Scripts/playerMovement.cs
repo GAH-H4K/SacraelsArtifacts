f5adfa95023685bd70176eb9d3a745b33fbbc1ce
Gabriel Otacilio <otakuobscuro.ag@gmail.com>
2026-05-16 00:51:22 -0300

    Nada mais

--- SacraelsArtifacts/Assets/Scripts/playerMovement.cs
+++ SacraelsArtifacts/Assets/Scripts/playerMovement.cs
@@ -1,15 +1,10 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
-
 public class playerMovement : MonoBehaviour
 {
     public float speed = 5f;
-    public float jumpForce = 10f;
-
     private Rigidbody2D rb;
-    private bool isGrounded;
 
-    private Vector2 moveInput;
+    private Vector1 movement;
 
     void Start()
     {
@@ -18,34 +13,6 @@ public class playerMovement : MonoBehaviour
 
     void Update()
     {
-        
-        rb.linearVelocity = new Vector2(moveInput.x * speed, rb.linearVelocity.y);
-
-        
-        if (Keyboard.current.spaceKey.wasPressedThisFrame && isGrounded)
-        {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-        }
-    }
-
-    void OnMove(InputValue value)
-    {
-        moveInput = value.Get<Vector2>();
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = true;
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = false;
-        }
+        movement.x = Input.GetAxisRaw("Horizontal");
     }
 }
