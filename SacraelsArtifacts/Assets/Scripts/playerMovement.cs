bc7d182b3207c8c3f5719e1ec40d0f2fdbda31ed
Gabriel Otacilio <otakuobscuro.ag@gmail.com>
2026-05-18 16:39:22 -0300

    movimento se mexe
    o personagem se mexe
    e comecei a fazer o script pra ele mudar de direçao

--- SacraelsArtifacts/Assets/Scripts/playerMovement.cs
+++ SacraelsArtifacts/Assets/Scripts/playerMovement.cs
@@ -2,27 +2,30 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float horizontalInput;
-
+       public float speed = 5f;
+ 
     private Rigidbody2D rb;
-
-    private int Speed;
-
-    private void Awake()
+    private float moveInput;
+ 
+    void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
-
+ 
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
+        moveInput = Input.GetAxisRaw("Horizontal");
 
+        if(moveInput > 0)
+        {
+            transfomr
+        }
+    
     }
-
-    private void FixedUpdate()
+ 
+    void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(horizontalInput * Speed, rb.linearVelocity.y);
-
+        rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
     }
 
 }
