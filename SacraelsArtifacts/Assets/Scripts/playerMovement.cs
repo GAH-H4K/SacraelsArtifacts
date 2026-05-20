3dc8b4d72a1c43b25887261387bf1bf7a0e913e4
Gabriel Otacilio <otakuobscuro.ag@gmail.com>
2026-05-20 14:39:15 -0300

    fmovimento

--- SacraelsArtifacts/Assets/Scripts/playerMovement.cs
+++ SacraelsArtifacts/Assets/Scripts/playerMovement.cs
@@ -1,11 +1,14 @@
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-       public float speed = 5f;
+    public float speed = 5f;
+    public float jumpForce = 10f;
  
     private Rigidbody2D rb;
     private float moveInput;
+    private bool isOnGround;
  
     void Start()
     {
@@ -16,13 +19,39 @@ public class PlayerMovement : MonoBehaviour
     {
         moveInput = Input.GetAxisRaw("Horizontal");
 
+        //linhas pra mexer o sprite pros lado(/?????)
         if(moveInput > 0)
         {
-            transfomr
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (moveInput < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround) ;
+        {
+        //    rb.linearVelocity = new Vector2(rb.linearVelocity.y);
         }
     
     }
- 
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = false;
+        }
+    }
+
     void FixedUpdate()
     {
         rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
