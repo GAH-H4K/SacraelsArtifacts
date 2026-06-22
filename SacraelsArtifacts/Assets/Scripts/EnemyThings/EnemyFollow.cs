f10d88b709ab9b529177c33b899761cd9424c068
Gabriel Otacilio <otakuobscuro.ag@gmail.com>
2026-06-22 16:40:07 -0300

    Enemy Follow Player
    n terminei

--- SacraelsArtifacts/Assets/Scripts/EnemyThings/EnemyFollow.cs
+++ SacraelsArtifacts/Assets/Scripts/EnemyThings/EnemyFollow.cs
@@ -1,10 +1,18 @@
-using UnityEngine;
 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 public class EnemyFollow : MonoBehaviour
 {
+
+    private Transform PlayerPosition;
+
+    public float EnemySpeed;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        PlayerPosition = GameObject.FindGameObjectWithTag("Player").transform;
         
     }
 
@@ -12,5 +20,10 @@ public class EnemyFollow : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void FollowPlayer()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, PlayerPosition);
     }
 }
