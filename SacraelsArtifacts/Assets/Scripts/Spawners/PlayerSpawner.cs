0cbdb30e1254d7137fc4300249500bbee13db2b0
Gabriel Otacilio <otakuobscuro.ag@gmail.com>
2026-08-02 19:36:10 -0300

    Terminei o menu de seleçao

--- SacraelsArtifacts/Assets/Scripts/Spawners/PlayerSpawner.cs
+++ SacraelsArtifacts/Assets/Scripts/Spawners/PlayerSpawner.cs
@@ -1,6 +1,18 @@
 using UnityEngine;
-
+using Unity.Cinemachine;
 public class PlayerSpawner : MonoBehaviour
 {
     public GameObject[] playerPrefabs; 
+
+    public Transform spawnPoint;
+    public CinemachineCamera virtualCamera;
+
+    private void Start()
+    {
+        int character = PlayerPrefs.GetInt("Character", 0);
+
+        GameObject player = Instantiate(playerPrefabs[character], spawnPoint.position, Quaternion.identity);
+
+        virtualCamera.Follow = player.transform;
+    }
 }
