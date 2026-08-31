88002f285d418ca25fbc051eebcae5b9c5847630
Gabriel Otacilio <otakuobscuro.ag@gmail.com>
2026-08-31 16:40:11 -0300

    Consertando a animaçao do boss

--- SacraelsArtifacts/Assets/Scripts/Enemy/Boss/BossActivation.cs
+++ SacraelsArtifacts/Assets/Scripts/Enemy/Boss/BossActivation.cs
@@ -2,9 +2,8 @@ using UnityEngine;
 
 public class BossActivation : MonoBehaviour
 {
-    public GameObject bossFightConfiner;
+    public GameObject bossFight;
 
-    public GameObject boss;
 
     public bool isBossFightActive = false;
 
@@ -12,41 +11,15 @@ public class BossActivation : MonoBehaviour
 
     public float TimeToFinishTheFight = 10f;//isso é o tempo pra desativar o garotao
 
-
-
-    private void Update()
-    {
-        timeToFinishTheFight();
-        IsFightOver();
-    }
-
-    public void timeToFinishTheFight()
-    {
-        if(TimeToFinishTheFight > 0 && isBossFightActive == false)
-        {
-           TimeToFinishTheFight -= Time.deltaTime;
-        }
-       
-    }
-
-    public void IsFightOver()
-    {
-        if(isBossFightActive == false && TimeToFinishTheFight <= 0)
-        {
-            bossFightConfiner.SetActive(false);
-            boss.SetActive(false);
-        }
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") && ifAlredyExecuted == false )
         {
-            bossFightConfiner.SetActive(true);
-            boss.SetActive(true);
+            bossFight.SetActive(true);
             ifAlredyExecuted = true;
             isBossFightActive = true;
         }
 
     }
+
 }
